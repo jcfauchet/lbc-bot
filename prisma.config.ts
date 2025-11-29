fe178923bd76051320e05443ffc65ef91626c8af
Jean-Charles FAUCHET <fauchet.jeancharles@gmail.com>
2025-11-30 00:27:12 +0100

    db

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: 'postgresql://postgres.xjytxesfwspjnqzgerlz:Yutw1uYx7U8gAE18@aws-1-eu-west-3.pooler.supabase.com:6543/postgres',
+    url: 'postgresql://postgres.xjytxesfwspjnqzgerlz:Yutw1uYx7U8gAE18@aws-1-eu-west-3.pooler.supabase.com:6543/postgres?sslmode=require',
     // shadowDatabaseUrl: 'postgresql://postgres:Yutw1uYx7U8gAE18@db.xjytxesfwspjnqzgerlz.supabase.co:5432/postgres?sslmode=require',
   },
 })
