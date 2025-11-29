41221a47f426913a6f5c46e76f3d190f3772db3c
Jean-Charles FAUCHET <fauchet.jeancharles@gmail.com>
2025-11-30 00:18:00 +0100

    db

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   },
   datasource: {
     url: process.env.DATABASE_URL as string,
+    directUrl: 'postgresql://postgres.xjytxesfwspjnqzgerlz:Yutw1uYx7U8gAE18@aws-1-eu-west-3.pooler.supabase.com:6543/postgres?sslmode=require',
   },
 })
 
