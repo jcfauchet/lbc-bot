70649c5516b20d44d5ca6dea85d8e4b09b49d7fd
Jean-Charles FAUCHET <fauchet.jeancharles@gmail.com>
2025-12-03 13:08:41 +0100

    feat: update cache duration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { container } from "@/infrastructure/di/container";
 import { format } from "date-fns";
 import Link from "next/link";
 
-export const revalidate = 3600;
+export const revalidate = 100;
 
 export default async function Home() {
   const stats = await container.getDashboardStatsUseCase.execute();
